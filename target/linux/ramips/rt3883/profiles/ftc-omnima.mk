9a349173ddc936ad795f053e3c1967353fd7a660
Jimi Xenidis <jimix1@qti.qualcomm.com>
2013-10-15 19:34:19 -0700

    FTC: Inital stab at an FTC specific profile

---
+++ target/linux/ramips/rt3883/profiles/ftc-omnima.mk
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2013 OpenWrt.org
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/FTCOMNIEMBHPM
+	NAME:=FTC Omnima EMB-HPM board
+	PACKAGES:=kmod-usb-core kmod-usb-ohci kmod-usb2
+	# we need these packages
+	PACKAGES+= libevent2
+	PACKAGES+= libusb-1.0
+	PACKAGES+= usbutils
+	PACKAGES+= usbreset
+	PACKAGES+= samuel
+	PACKAGES+= dmesg
+	PACKAGES+= uboot-envtools
+	PACKAGES+= kmod-hid
+	PACKAGES+= kmod-input-joydev
+	PACKAGES+= kmod-input-joystick
+	PACKAGES+= kmod-input-joystick-xpad
+	PACKAGES+= kmod-usb-hid
+	PACKAGES+= kmod-usbmon
+	# remove pacakages we don't want
+	PACKAGES+= -dnsmasq
+	PACKAGES+= -firewall
+	PACKAGES+= -kmod-iptables
+	PACKAGES+= -kmod-ip6tables
+	PACKAGES+= -kmod-ppp
+	PACKAGES+= -kmod-6relayd
+	# remove router packages
+	PACKAGES+=$(DEFAULT_PACKAGES.router:%=-%)
+endef
+
+define Profile/FTCOMNIEMBHPM/Description
+	FTC Profile for using Omnima HPM with the LEGO NXT Brick.
+	This is particularly targeted for use with the Samuel Project.
+endef
+$(eval $(call Profile,FTCOMNIEMBHPM))
